0012504c28f7d6c6aa5efa4c5d00bc369480ec56
shubhajit12 <118266831+shubhajit12@users.noreply.github.com>
2023-03-13 20:34:46 +0530

    Add files via upload

---
+++ gamepage.js
@@ -0,0 +1,27 @@
+// random value generated
+var player_name=localStorage.getItem("player_name");
+  var y=Math.floor(Math.random()*10+1);
+// counting the number of guesses
+// made for correct Guess
+var guess=1;
+function submit(){
+    var x=document.getElementById("guessField").value;
+    if(x==y){
+        alert("CONGRATULATION!! "+player_name+" YOU GUESSED IT RIGHT IN "+guess+" GUESS ");
+        guess=1;
+    }
+    else if(x>y){
+      guess++;
+      alert(" OOPS SORRY!!! TRY A SMALLER NUMBER");
+    }
+    else{
+        guess++;
+        alert(" OOPS SORRY!!! TRY A GREATER NUMBER");
+    }
+}
+
+function playAgain(){
+    y=Math.floor(Math.random()*10+1);
+}
+  
+// function for number guessed by user     
